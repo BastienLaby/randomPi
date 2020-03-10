25961798d47f77ecb3f9103847bc13b4a5212112
Bastien Laby <bastien.laby@gmail.com>
2020-03-10 23:59:33 +0100

    black format

--- randomPi.py
+++ randomPi.py
@@ -1,20 +1,23 @@
 import random
 
-'''
+"""
 Given one only function random.random() which produces a uniformly generated number, calculate PI.
 
 Circle area of radius 1 : C = PI * r^2 = PI
 Area of a square of size 2 : S = (2 * r)^2 = 4
 Generate X 2D points (x, y) with both x and y in [0, 1]
 Finally, C / S = number of points in the circle / number of points in the square.
-'''
+"""
+
 
 def estimatePi(iterations):
     pi = 0
     circlePointsCount = 0
     for i in range(iterations):
         x, y = random.random(), random.random()
-        if x ** 2 + y ** 2 <= 1: # no need to sqrt() because if x < 1, sqrt(x) < 1 and if x > 1, sqrt(x) > 1
+        if (
+            x ** 2 + y ** 2 <= 1
+        ):  # no need to sqrt() because if x < 1, sqrt(x) < 1 and if x > 1, sqrt(x) > 1
             circlePointsCount += 1
     return 4 * circlePointsCount / iterations
 
